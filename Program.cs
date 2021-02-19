f0585ae785bc3d0c341376ee8c9d4340659de86a
Matthew Wood <matthew.wood_28@outlook.com>
2021-02-19 07:06:27 -0800

    Completed Algorithm

--- Program.cs
+++ Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Simple_Array_Sum
 {
     class Program
     {
+        /*
+        * Complete the simpleArraySum function below.
+        */
+
+        static int simpleArraySum(int[] ar)
+        {
+            int n = 0;
+
+            foreach (int num in ar)
+            {
+                
+                n += num;
+            }
+            return n;
+
+        }
+
         static void Main(string[] args)
         {
-            
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            ;
+            int result = simpleArraySum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
